d58a4f4ed304851ffa193a127b537b5d7d26b746
6v6 <injeong410@gmail.com>
2021-02-28 18:40:29 +0900

    test

--- cornview/src/main/java/com/ds/controller/HomeController.java
+++ cornview/src/main/java/com/ds/controller/HomeController.java
@@ -61,7 +61,7 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTme", formattedDate );
 		
 		return "main";
 	}
